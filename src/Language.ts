bc0a783361c27fa5c06abfe393a5b8c4fcc80e74
Lukaribou <lucasperroud17@gmail.com>
2020-11-21 10:20:25 +0100

    Update Language.ts

--- src/Language.ts
+++ src/Language.ts
@@ -1,26 +1,24 @@
 export interface ILanguage {
     code: string;
+    name: string;
     data: Object;
 }
 
 export class Language {
-    private _code: string;
+    private readonly _code: string;
     private readonly _data: Object;
+    private readonly _name: string;
 
     get code(): string {
         return this._code;
     }
 
-    set code(code: string): void {
-        this._code = code;
+    get name(): string {
+        return this._name;
     }
 
     get data(): Object {
         return this._data;
-    }
-
-    get length(): number {
-        return Object.keys(this._data).length;
     }
 
     get fields(): string[] {
@@ -32,7 +30,30 @@ export class Language {
         this._data = data;
     }
 
+    /**
+     * Returns a Language object from an Object that matches the ILanguage interface
+     * @param o
+     */
     public static fromObject(o: ILanguage): Language {
         return new Language(o.code, o.data);
+    }
+
+    public hasField(fieldName: string): boolean {
+        return this.fields.includes(fieldName);
+    }
+
+    /**
+     * Gets the value (so the traduction)
+     * @param fieldName
+     */
+    public getValue(fieldName: string): string {
+        return this.fields.includes(fieldName) ? this._data[fieldName] : "Not provided";
+    }
+
+    /**
+     * Lists the fields where the value is empty
+     */
+    public getEmptyFields(): string[] {
+        return this.fields.filter((field) => this._data[field] === '');
     }
 }
